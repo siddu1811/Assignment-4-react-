676af75d5e628aa25c2e413fd68bac5c3ea742a8
siddu1811 <148976942+siddu1811@users.noreply.github.com>
2023-10-26 18:16:58 +0530

    Update SpellGrammarCheck.js

--- SpellGrammarCheck.js
+++ SpellGrammarCheck.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import './TextGearsChecker.css';
 
 const TextGearsChecker = () => {
   const [inputText, setInputText] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [selectedErrorIndex] = useState(null);
+  const [selectedSuggestionIndex] = useState(null);
 
   const handleCheck = async () => {
     try {
@@ -12,45 +15,70 @@ const TextGearsChecker = () => {
         `text=${inputText}`,
         {
           params: {
-            key: 'u7LwMpX4u1xA7f3n',
+            language: 'en-GB',
+            key: 'uA6hWUQ9eJmzN7L8',
           },
         }
       );
-      console.log(response);
-
-      if (response.data.errors) {
-        const misspelledWords = response.data.errors;
-        const suggestedWords = misspelledWords.map((error) => error.better.join(', '));
-        setSuggestions(suggestedWords);
-      } else {
-        setSuggestions(['No suggestions found.']);
-      }
+      const errors = response.data.response.errors;
+      setSuggestions(errors);
     } catch (error) {
       console.error('Error checking text:', error);
-      setSuggestions(['Error occurred while checking text.']);
+      setSuggestions([{ error: 'Error occurred while checking text.', better: [] }]);
     }
   };
 
+  const handleSuggestionClick =(index, suggestion) => {
+    const updatedText = inputText.split(' ');
+    updatedText[index] = suggestion;
+    console.log(updatedText);
+    setInputText(updatedText.join(' '));
+    console.log(index);
+    console.log(suggestion);
+    
+
+   
+  };
   return (
-    <div>
+    <div className="textgears-checker-container">
       <h1>TextGears Spell and Grammar Checker</h1>
       <textarea
+        className="text-input"
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
         placeholder="Enter your text here..."
       />
-      <button onClick={handleCheck}>Check</button>
-      <div>
-        {suggestions.length > 0 && (
-          <div>
-            <h2>Suggestions:</h2>
-            <ul>
-              {suggestions.map((suggestion, index) => (
-                <li key={index}>{suggestion}</li>
-              ))}
-            </ul>
-          </div>
-        )}
+      <button className="check-button" onClick={handleCheck}>
+        Check
+      </button>
+      <div className="suggestions-container">
+        <div>
+          <h2>Better Words:</h2>
+          <ul>
+            {suggestions.map((error, errorIndex) => (
+              <li key={errorIndex}>
+                <span
+                  className={`error-text ${selectedErrorIndex === errorIndex ? 'selected-error' : ''}`}
+                  onClick={() => handleSuggestionClick(errorIndex, null)}
+                >
+                  {error.error}
+                </span>
+                <ul>
+                  {error.better.map((suggestion, suggestionIndex) => (
+                    <li key={suggestionIndex}>
+                      <button
+                        onClick={() => handleSuggestionClick(errorIndex, suggestion)}
+                        className={`suggestion-button ${selectedSuggestionIndex === suggestionIndex && selectedErrorIndex === errorIndex ? 'selected-suggestion' : ''}`}
+                      >
+                        {suggestion}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
